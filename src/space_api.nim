e56a7fc10b8bd273fedfef64c06dc0859e959d85
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2020-06-07 22:08:45 +0100

    Simple postgresql database wrapper.

--- src/space_api.nim
+++ src/space_api.nim
@@ -1,9 +1,16 @@
-import space_api/transaction
+import space_api/transaction, space_api/database
 
-proc loadTransactions(): void =
+proc loadTransactions(): Transaction =
   var transactions: Transaction = createNewTransaction(1, "deliveroo payment for pizza", 23.33, "05/06/20")
-  echo(transactions)
+  return transactions
 
 when isMainModule:
   echo("Space API started...")
-  loadTransactions()
+  let space_core_database: auto = db_init()
+
+  echo(loadTransactions())
+  let version: string = db_get_version(space_core_database)
+  
+  echo(version)
+
+  db_close(space_core_database)
---
+++ src/space_api/database.nim
@@ -0,0 +1,12 @@
+import db_postgres
+
+# TODO: load values from env
+proc db_init*(): DbConn =
+    let db: DbConn = open("localhost", "local_user", "this_is_not_a_password", "space_core_db")
+    return db
+
+proc db_get_version*(db: DbConn): string =
+    result = db.getValue(sql"SELECT version();")
+
+proc db_close*(db: DbConn): void =
+    db.close()
